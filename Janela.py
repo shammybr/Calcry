b0ce0a174b3b7c98fd5bd806682de3d2ca27ba9a
Gabriel Costa <gabrielcostads08@gmail.com>
2025-12-05 16:35:45 -0300

    Nome da janela: Calcry

--- Janela.py
+++ Janela.py
@@ -5,7 +5,7 @@ def CriarJanela():
 
     screen = pygame.display.set_mode((1280, 720))
     pygame.draw.line(screen, (0, 100, 0), (0, 500), (600, 500), 3) # linha do horizonte
-    janela.set_title("Joguinho")
+    janela.set_title("Calcry")
     return janela
 
 
